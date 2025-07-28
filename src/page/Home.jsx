b249a45b07d6df4de1c233ffb59d8f1cee369250
Dabsia <dabbojohnson98@gmail.com>
2025-07-28 21:02:13 +0100

    Made some changes

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import useEffect from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import MoveDetails from "../components/MoveDetails";
