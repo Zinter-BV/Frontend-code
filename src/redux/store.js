1048b3fd89428c1006fa46e99dc2f994a17d6798
Dabsia <dabojohnson98@gmail.com>
2026-03-13 21:51:47 +0100

    made some changes to remove the logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,24 @@
+// import rootReducer from "./rootReducer";
+// import { createStore, applyMiddleware } from "redux";
+// import logger from "redux-logger";
+// import { persistStore } from "redux-persist";
+
+// const middlewares = [logger];
+
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// export const persistor = persistStore(store);
+
 import rootReducer from "./rootReducer";
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV !== "production") {
+  const { createLogger } = require("redux-logger");
+  middlewares.push(createLogger());
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
